342c88dae190be23c813f2177eff42cef2ed9004
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-08 21:34:15 -0700

    Loops - the basics complete

--- js/reps.js
+++ js/reps.js
@@ -149,4 +149,24 @@ const canDrive = function(person) {
     }
 }
 
-canDrive(licenseAge);
+// canDrive(licenseAge);
+
+
+
+// SECTION - LOOPS: The Basics
+
+// 1. write a loop that will print out all the numbers from 0 to 10, inclusive.
+// for (let a = 0; a <= 10; a++) {
+//     console.log(a)
+// }
+
+// 2. Write a loop that will print out all the numbers from 10 up to and including 400.
+// for (let a = 0; a <= 400; a++) {
+//     console.log(a);
+// }
+
+// 3. Write a loop that will print out every third number starting with 11 and go no higher than 400.
+for (let a = 11; a <= 400; a += 3) {
+    console.log(a);
+}
+
