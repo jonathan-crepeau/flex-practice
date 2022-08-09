51e4414032ed0fc5a2ec72a0ee1ee02fcd3cf461
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-09 12:40:39 -0700

    Loops - Multiples of 3 & 5 complete

--- js/reps.js
+++ js/reps.js
@@ -218,3 +218,21 @@ const canDrive = function(person) {
 // }
 
 // console.log(bank_account);
+
+
+
+// SECTION - LOOPS: Multiples of 3 & 5
+
+// 1. If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, 9. Find the sume of all the multiples of 3 or 5 below 1000.
+
+// let sum = 0;
+
+// for (let a = 0; a < 1000; a++) {
+//     if (a % 3 == 0) {
+//         sum += a;
+//     } if (a % 5 == 0) {
+//         sum += a;
+//     }
+// }
+
+// console.log(sum);
